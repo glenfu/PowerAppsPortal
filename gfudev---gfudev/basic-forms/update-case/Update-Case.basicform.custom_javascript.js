aa543c74dd264061586d2ec0d279fa46236f1169
glenfu <guoyong.fu@gmail.com>
2022-08-04 14:50:21 +0800

    Add customizations like Edit Partner, CSS, case list and dynamic content

---
+++ gfudev---gfudev/basic-forms/update-case/Update-Case.basicform.custom_javascript.js
@@ -0,0 +1,5 @@
+$(function(){
+    $(".btn.btn-default.launchentitylookup").attr("data-original-title","Search");
+    $(".btn.btn-default.launchentitylookup").attr("title","Search");
+    $(".btn.btn-default.launchentitylookup").attr("aria-label","Search");
+});
