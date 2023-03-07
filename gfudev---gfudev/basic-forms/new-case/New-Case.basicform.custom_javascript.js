4678c6abd3326fb5cc605ea0bf157965c6834907
glenfu <guoyong.fu@gmail.com>
2023-03-07 17:13:57 +0800

    Updated table permissions to reflect custom portal team N:N relationship with contact and added RTE to case form

--- gfudev---gfudev/basic-forms/new-case/New-Case.basicform.custom_javascript.js
+++ gfudev---gfudev/basic-forms/new-case/New-Case.basicform.custom_javascript.js
@@ -15,4 +15,39 @@
           $("#primarycontactid_entityname").val("contact");
           $("#primarycontactid").trigger("change");
         }
+
+        ConvertSelectToAutocomplete("con_manager");
   });
+
+  function ConvertSelectToAutocomplete(selectName, selectPlaceholder) {
+    selectPlaceholder = selectPlaceholder ?? "";
+    var selectElement = $("#" + selectName);
+    var selectElementClass = selectElement.attr("class");
+    var readonly = $(selectElement).attr("readonly") ?? "";
+    var autoCompleteElementId = selectName + "-autocomplete";
+    var autoCompleteDatasetId = selectName + "-data";
+    var autoCompleteElement = '<input name="' + autoCompleteElementId + '" id="' + autoCompleteElementId + '" class="' + selectElementClass + '" list="' + autoCompleteDatasetId + '" placeholder="' + selectPlaceholder + '" ' + readonly + '><datalist id="' + selectName + '-data"></datalist>';
+    var options = "";
+ 
+    $(selectElement).parent().append(autoCompleteElement);
+    $("#" + selectName + " option").each(function (index, o) {
+        options += '<option data-value="' + o.value + '" value="' + o.text + '"/>';
+    });
+    $("#" + autoCompleteDatasetId).html(options);
+ 
+    $(selectElement).hide();
+ 
+    var currentSelectedValue = $(selectElement).val();
+    if (!!currentSelectedValue) {
+        $("#" + autoCompleteElementId).val($(selectElement).find("option:selected").text());
+    }
+ 
+    $("#" + autoCompleteElementId).on("change", function () {
+        var selectedValue = $("#" + autoCompleteDatasetId + " option[value='" + $("#" + autoCompleteElementId).val() + "']").attr("data-value");
+        selectElement.val(selectedValue);
+        if (typeof selectedValue === "undefined") {
+            $("#" + autoCompleteElementId).val("");
+            // optionally you can add an error message here
+        };
+    });
+};
