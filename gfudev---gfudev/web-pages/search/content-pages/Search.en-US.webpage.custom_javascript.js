ee7ed008dcb88b2e465d674ea721a33e912ace14
glenfu <guoyong.fu@gmail.com>
2023-04-20 18:35:25 +0800

    react webpack testing, append url parameters

---
+++ gfudev---gfudev/web-pages/search/content-pages/Search.en-US.webpage.custom_javascript.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+//     var url = new URL(window.location.href);
+//     // url.append('filter', 'abc');
+//     // window.location.replace('?' + url.toString()); 
+//     url.searchParams.set('filter', 'abc');
+//     history.pushState({}, "", url); 
+// });
