f8d2e8b0530efb15e73b0cf649a6a1400c669e86
glenfu <guoyong.fu@gmail.com>
2023-04-24 18:26:48 +0800

    reverted react code and added search filter query to endpoint

--- gfudev---gfudev/web-pages/search/content-pages/Search.en-US.webpage.custom_javascript.js
+++
@@ -1,7 +0,0 @@
-// $(document).ready(function() {
-//     var url = new URL(window.location.href);
-//     // url.append('filter', 'abc');
-//     // window.location.replace('?' + url.toString()); 
-//     url.searchParams.set('filter', 'abc');
-//     history.pushState({}, "", url); 
-// });
